1c1c073acc6169264108e10f77ec88e6258a70a9
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2023-02-13 15:42:43 +0500

    feat: 02-11 AppRouter. Конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,27 @@ import {classNames} from "shared/lib/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 
 import "./styles/index.scss";
+import {routerConfig} from "app/providers/router/routeConfig";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={classNames("app", {hovered: true, selectable: true, some: true}, [theme, "class1 class2"])}>
+        <div className={classNames("app", {}, [theme])}>
             <button type="button" onClick={toggleTheme}>Theme</button>
             <Link to={"/"}>Home</Link>
             <Link to={"/about"}>About</Link>
             <Suspense fallback={"Loading..."}>
                 <Routes>
+                    {
+                        Object.values(routerConfig).map(({path, element}) => (
+                            <Route
+                                key={path}
+                                path={path}
+                                element={element}
+                            />
+                        ))
+                    }
                     <Route path={"/"} element={<MainPage />} />
                     <Route path={"/about"} element={<AboutPage />} />
                 </Routes>
---
+++ src/app/providers/router/routeConfig/index.ts
@@ -0,0 +1,1 @@
+export { routerConfig } from "./ui/routeConfig";
