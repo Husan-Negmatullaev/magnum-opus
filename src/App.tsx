67c8472956009beef0509097f3aca3e07abdca53
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2023-02-13 10:41:36 +0500

    feat: Организация стиÐ01-07: Организация стÐлей. Добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutAsyncPage} from "./pages/AboutPage/AboutPage.async";
 import {MainAsyncPage} from "./pages/MainPage/MainPage.async";
 
-import "./index.scss";
+import {useTheme} from "./theme/useTheme";
+
+import "./styles/index.scss";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <>
+        <div className={`app ${theme}`}>
+            <button type="button" onClick={toggleTheme}>Theme</button>
             <Link to={"/"}>Home</Link>
             <Link to={"/about"}>About</Link>
             <Suspense fallback={"Loading..."}>
@@ -17,7 +22,7 @@ const App = () => {
                     <Route path={"/about"} element={<AboutAsyncPage />} />
                 </Routes>
             </Suspense>
-        </>
+        </div>
     );
 };
 
---
+++ src/theme/useTheme.ts
@@ -0,0 +1,22 @@
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+import React from "react";
+
+interface IUseTheme {
+    theme: Theme;
+    toggleTheme: () => void;
+}
+
+export const useTheme = (): IUseTheme => {
+    const {theme, setTheme} = React.useContext(ThemeContext);
+
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
+        setTheme(newTheme);
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+    };
+
+    return {
+        theme,
+        toggleTheme
+    };
+}
