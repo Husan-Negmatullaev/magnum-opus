68ffeaf5aea0151c857a8ea3a00460ff5aa4a9fb
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2023-03-13 00:55:23 +0500

    fix: 04-33 Husky. Pre commit хуки

---
+++ dist/733.8e1028beb70c410dc456.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmagnum_opus=self.webpackChunkmagnum_opus||[]).push([[733],{733:(e,n,u)=>{u.r(n),u.d(n,{default:()=>h});var a=u(893),r=u(443),s=$RefreshSig$(),t=function(){s();var e=(0,r.$)("home").t;return(0,a.jsx)("div",{children:e("home:main-text")})};s(t,"h6J0Q3nxDyaAQ99JMz6OOoWbcwM=",!1,(function(){return[r.$]}));const h=t;$RefreshReg$(t,"MainPage")}}]);
