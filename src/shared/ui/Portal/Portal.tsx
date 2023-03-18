e139396af4c42a475c3ff5057980cd8e052a9c88
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2023-03-18 19:51:24 +0500

    fix: 05-35 Оптимизация. Асинхронные редюсеры. Размер бандла

--- src/shared/ui/Portal/Portal.tsx
+++ src/shared/ui/Portal/Portal.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 
 interface PortalProps {
     children: ReactNode,
-    container?: HTMLElement
 }
 
 export const Portal: React.FC<PortalProps> = (props) => {
   const {
     children,
-    container = document.body,
   } = props;
 
-  return ReactDOM.createPortal(children, container);
+  const rootElement = React.useRef<HTMLDivElement>(null);
+  const [isMounted, setIsMounted] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    rootElement.current = document.querySelector('#root');
+    setIsMounted(true);
+  }, []);
+
+  return isMounted && rootElement.current
+    ? ReactDOM.createPortal(children, rootElement.current)
+    : null;
 };
